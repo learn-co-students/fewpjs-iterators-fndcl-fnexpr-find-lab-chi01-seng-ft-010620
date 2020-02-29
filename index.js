d34bf0a2caac1a193e78bb7f9cec708c385156fb
Andrea <andreabarr87@gmail.com>
2020-02-29 16:18:00 -0600

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+const superbowlWin = (record) => {
+  const result = record.find( record => record.result === 'W' );
+  return !!result ? result.year : undefined
 }
